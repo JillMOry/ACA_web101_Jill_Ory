0e73408687cce4ff21c612db7cb6f2b7a94e0b62
JillMOry <oryjill@gmail.com>
2019-12-15 20:44:25 -0600

    towerOfHanoi commit

--- src/Scripts/towerOfHanoi.js
+++ src/Scripts/towerOfHanoi.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var x;
 	$(".piece").click(function() {
-		x = $(".piece:last").detach();
+		x = $(this).detach(); //used ".piece:last" but that only worked to move the items into the colored bars then it broke.  this points to the element rather than the last  piece.  detach hold the element in memory for use later.
 	});
 
 	$(".tower").dblclick(function() {
